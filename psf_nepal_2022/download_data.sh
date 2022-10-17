3c741f2b6d99cf9fd95b6845655738c2f0d497bc
adehecq <3285905+adehecq@users.noreply.github.com>
2022-10-17 14:49:46 +0200

    Create data folder

--- psf_nepal_2022/download_data.sh
+++ psf_nepal_2022/download_data.sh
@@ -4,5 +4,6 @@
 
 wget --user-agent Mozilla/4.0 'https://filesender.renater.fr/download.php?token=5aab3c13-cdfb-41f5-a930-8da6368e2659&archive_format=undefined&files_ids=18517548' -O mera_data.tgz
 tar xzvf mera_data.tgz
+mkdir -p psf_nepal_2022/data
 mv xdem_mera_data/ psf_nepal_2022/data/mb_Mera
 rm mera_data.tgz
