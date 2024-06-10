0e8de9c15193b7f082e42ee9c7659f96aa4fd6bf
adehecq <3285905+adehecq@users.noreply.github.com>
2024-06-10 16:11:06 +0200

    Amend previous commit

--- download_data.sh
+++ download_data.sh
@@ -4,3 +4,4 @@
 
 wget --user-agent Mozilla/4.0 'https://filesender.renater.fr/download.php?token=6a412638-b939-4bd1-93eb-d91269af3ae5&files_ids=39705894' -O mera_data.tgz
 tar xzvf mera_data.tgz
+rm mera_data.tgz
