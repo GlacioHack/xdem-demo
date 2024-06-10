1a5e24d125aa7b376fab6ad51ece89a296648f20
adehecq <3285905+adehecq@users.noreply.github.com>
2024-06-10 16:05:38 +0200

    Update download script

--- download_data.sh
+++ download_data.sh
@@ -2,8 +2,5 @@
 # The link is found by downloading it with a browser, then in Download, right-click on the data and copy download URL.
 # See https://www.forbesconrad.com/blog/download-wetransfer-to-linux-server-wget/
 
-wget --user-agent Mozilla/4.0 'https://filesender.renater.fr/download.php?token=5aab3c13-cdfb-41f5-a930-8da6368e2659&archive_format=undefined&files_ids=18517548' -O mera_data.tgz
+wget --user-agent Mozilla/4.0 'https://filesender.renater.fr/download.php?token=6a412638-b939-4bd1-93eb-d91269af3ae5&files_ids=39705894' -O mera_data.tgz
 tar xzvf mera_data.tgz
-mkdir -p psf_nepal_2022/data
-mv xdem_mera_data/ psf_nepal_2022/data/mb_Mera
-rm mera_data.tgz
