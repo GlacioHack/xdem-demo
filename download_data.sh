31c12e8694612949187855df8516f550bbc48fb6
adehecq <3285905+adehecq@users.noreply.github.com>
2024-09-12 11:36:08 +0200

    Update postbuild

--- download_data.sh
+++ download_data.sh
@@ -2,6 +2,6 @@
 # The link is found by downloading it with a browser, then in Download, right-click on the data and copy download URL.
 # See https://www.forbesconrad.com/blog/download-wetransfer-to-linux-server-wget/
 
-wget --user-agent Mozilla/4.0 'https://filesender.renater.fr/download.php?token=bc73932e-4999-4237-8554-2e1a8862fcd2&files_ids=39956953' -O mera_data.tgz
+wget  --user-agent Mozilla/4.0 'https://filesender.renater.fr/download.php?token=00e59d13-98dc-4b7f-9ffd-92bb28e06987&files_ids=43430563' -O mera_data.tgz
 tar xzvf mera_data.tgz
 rm mera_data.tgz
